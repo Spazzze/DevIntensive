43fdca9a15a1b2afb0a01e668eaac3bac4810aee
Spazzze <elena.space@gmail.com>
2016-06-23 12:42:50 +0300

    added MainActivity's lifecycle with logging; added some new colors; fixed dimens; created MainActivity's screen design with new styles, icons and NestedScrollView ofc.

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -1,15 +1,81 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+
+    /**
+     * метод вызывается при создании активити (после изменения/возврата к текущей
+     * активности после ее уничтожения)
+     *
+     * в данном методе инициализируется/производится:
+     * - UI statics;
+     * - init activity's static data;
+     * - link data to lists (init adapters);
+     *
+     * DO NOT EXECUTE LONGTIME OPERATIONS IN THIS METHOD!!!
+     *
+     * @param savedInstanceState - объект со значениями, сохраненными в Bundle - состояние UI
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+/*        if (savedInstanceState == null) {
+            //activity запущено впервые
+        } else {
+            //activity уже создавалось
+        }*/
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
