e47d4ae62719d23a5ceb142d6d856aaa9460cc16
Spazzze <elena.space@gmail.com>
2016-07-07 13:52:47 +0300

    implemented authbox using cardview

--- app/src/main/java/com/softdesign/devintensive/ui/activities/AuthorizationActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/AuthorizationActivity.java
@@ -1,11 +1,9 @@
 package com.softdesign.devintensive.ui.activities;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.softdesign.devintensive.R;
@@ -17,7 +15,6 @@ import butterknife.ButterKnife;
 public class AuthorizationActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String TAG = ConstantManager.TAG_PREFIX + "Auth Activity";
-    @BindView(R.id.authorization_box) LinearLayout mLinearLayout_authBox;
     @BindView(R.id.auth_enter_button) Button mButton_authenticate;
     @BindView(R.id.forgot_pass_button) TextView mButton_forgot_pass;
 
@@ -29,13 +26,6 @@ public class AuthorizationActivity extends AppCompatActivity implements View.OnC
 
         mButton_authenticate.setOnClickListener(this);
         mButton_forgot_pass.setOnClickListener(this);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            mLinearLayout_authBox.setElevation(getResources().getDimension(R.dimen.size_small_8));
-        } else {
-            //deprecated in API 22 но мы то это запустим в АПИ ниже 21, так что все как надо.
-            mLinearLayout_authBox.setBackground(getResources().getDrawable(android.R.drawable.dialog_holo_light_frame));
-        }
     }
 
     @Override
