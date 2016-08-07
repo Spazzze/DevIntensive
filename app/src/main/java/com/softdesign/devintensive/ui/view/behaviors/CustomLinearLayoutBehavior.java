5f81de365c8bf1266e47b23ea79b4f5fcd216c82
Spazzze <elena.space@gmail.com>
2016-08-07 20:53:08 +0300

    - переделка бихейвиора в надежде что за серую плашку будет тянуться и аппбар, но нет.

--- app/src/main/java/com/softdesign/devintensive/ui/view/behaviors/CustomLinearLayoutBehavior.java
+++ app/src/main/java/com/softdesign/devintensive/ui/view/behaviors/CustomLinearLayoutBehavior.java
@@ -2,22 +2,25 @@ package com.softdesign.devintensive.ui.view.behaviors;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Bundle;
-import android.os.Parcelable;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.softdesign.devintensive.R;
+import com.softdesign.devintensive.utils.AppUtils;
 
 import static com.softdesign.devintensive.utils.AppUtils.getAppBarSize;
 import static com.softdesign.devintensive.utils.AppUtils.getStatusBarHeight;
-import static com.softdesign.devintensive.utils.AppUtils.getViewMinHeight;
 
-public class CustomLinearLayoutBehavior<LinearLayout extends View> extends CoordinatorLayout.Behavior<LinearLayout> {
+public class CustomLinearLayoutBehavior extends AppBarLayout.ScrollingViewBehavior {
 
-    private float mMinLLSize, mMaxLLSize, mMinAppbarHeight, mMaxAppbarHeight, mExpandedPercentageFactor;
+    private float mMinLLSize;
+    private float mMaxLLSize;
+    private float mMinAppbarHeight;
+    private float mMaxAppbarHeight;
 
+    @SuppressWarnings({"unused"})
     public CustomLinearLayoutBehavior(Context context, AttributeSet attrs) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CustomLinearLayoutBehavior);
         mMinLLSize = a.getDimensionPixelSize(R.styleable.CustomLinearLayoutBehavior_min_collapsed_height, 0);
@@ -26,43 +29,21 @@ public class CustomLinearLayoutBehavior<LinearLayout extends View> extends Coord
     }
 
     @Override
-    public Parcelable onSaveInstanceState(CoordinatorLayout parent, LinearLayout child) {
-        Bundle bundle = new Bundle();
-        bundle.putFloat("mMinLLSize", this.mMinLLSize);
-        bundle.putFloat("mMaxLLSize", this.mMaxLLSize);
-        bundle.putFloat("mExpandedPercentageFactor", this.mExpandedPercentageFactor);
-        bundle.putFloat("mMaxAppbarHeight", this.mMaxAppbarHeight);
-        bundle.putFloat("mMinAppbarHeight", this.mMinAppbarHeight);
-        return bundle;
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
     }
 
     @Override
-    public void onRestoreInstanceState(CoordinatorLayout parent, LinearLayout child, Parcelable state) {
-        if (!(state instanceof Bundle)) return; // implicit null check
-
-        Bundle bundle = (Bundle) state;
-        this.mExpandedPercentageFactor = bundle.getFloat("mExpandedPercentageFactor");
-        this.mMinLLSize = bundle.getFloat("mMinLLSize");
-        this.mMaxLLSize = bundle.getFloat("mMaxLLSize");
-        this.mMinAppbarHeight = bundle.getFloat("mMinAppbarHeight");
-        this.mMaxAppbarHeight = bundle.getFloat("mMaxAppbarHeight");
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
         final CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
-        lp.height = (int) (mMinLLSize + (mMaxLLSize - mMinLLSize) * mExpandedPercentageFactor);
-        child.setLayoutParams(lp);
-    }
-
-    @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout child, View dependency) {
-        final CoordinatorLayout.LayoutParams lp =
-                (CoordinatorLayout.LayoutParams) child.getLayoutParams();
 
         if (mMinAppbarHeight == 0.0f) {
             initProperties(child, dependency);
         }
 
         float curAppBarHeight = dependency.getBottom() - mMinAppbarHeight;
-        mExpandedPercentageFactor = curAppBarHeight / mMaxAppbarHeight;
-        lp.height = (int) (mMinLLSize + (mMaxLLSize - mMinLLSize) * mExpandedPercentageFactor);
+        float expandedPercentageFactor = curAppBarHeight / mMaxAppbarHeight;
+        lp.height = (int) (mMinLLSize + (mMaxLLSize - mMinLLSize) * expandedPercentageFactor);
 
         child.setLayoutParams(lp);
 
@@ -71,7 +52,7 @@ public class CustomLinearLayoutBehavior<LinearLayout extends View> extends Coord
 
     private void initProperties(View child, View dependency) {  //расчет начальных параметров
         if (mMaxLLSize == 0.0f) mMaxLLSize = child.getHeight();
-        if (mMinLLSize == 0.0f) mMinLLSize = getViewMinHeight(child);
+        if (mMinLLSize == 0.0f) mMinLLSize = AppUtils.getViewMinHeight(child);
         mMinAppbarHeight = getStatusBarHeight() + getAppBarSize();
         mMaxAppbarHeight = dependency.getHeight() - mMinAppbarHeight;
     }
